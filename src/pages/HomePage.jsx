0849c7b7595e375b10dfd5408ae484ffc9e40f55
wahyuPM <wahyupratama191@gmail.com>
2022-11-30 12:05:26 +0800

    new : folder structure & react router

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react'
+
+import Header from '@/parts/Header'
+export default function HomePage(props) {
+  return (
+    <Fragment>
+        <Header></Header>
+    </Fragment>
+  )
+}
